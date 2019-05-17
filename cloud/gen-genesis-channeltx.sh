e79d5f7773e4253d1e0679ac8336cb97e2054cf1
pratikhyperfabrics <pratik.patil@hyperfabrics.com>
2019-05-17 13:20:45 -0400

    cloud enviorment setup

---
+++ cloud/gen-genesis-channeltx.sh
@@ -0,0 +1,22 @@
+# Copyright 2018 @ http://ACloudFan.com 
+# Part of a online course. Please check it out at http://www.acloudfan.com
+
+# Script for the generation of the 
+# genesis block & create channel tx files
+# Both files written under the artefacts subfolder
+
+
+#1 Generate the Genesis block
+export FABRIC_CFG_PATH=$PWD
+GENESIS_FILE=./artefacts/commercialpaper-genesis.block
+CHANNEL_NAME=ordererchannel
+echo "====> Generating Genesis"
+configtxgen -profile CommercialPaperOrdererGenesis -channelID $CHANNEL_NAME -outputBlock $GENESIS_FILE
+
+#3 Generate the airline channel transaction file
+CHANNEL_TX_FILE=./artefacts/commercialpaper-channel.tx
+CHANNEL_NAME=commercialpaperchannel
+echo "====> Generating Channel Tx"
+configtxgen -profile CommercialPaperChannel -outputCreateChannelTx $CHANNEL_TX_FILE -channelID $CHANNEL_NAME
+
+
---
+++ cloud/set-env.sh
@@ -0,0 +1,41 @@
+# Copyright 2018 @ http://ACloudFan.com 
+# Part of a online course. Please check it out at http://www.acloudfan.com
+
+# This sets the environment for accessing the cloud
+
+# Make sure that IP Addresses are setup properly
+# Use PUBLIC IP Here
+
+
+export ORDERER_ADDRESS=3.16.183.82:7050
+export DIGIBANK_EP=18.224.150.1:7051
+export MAGNETOCORP_EP=18.191.7.45:7051
+
+export CA_IP=54.173.103.244
+export CA_URL=http://$CA_IP:7054
+
+
+export FABRIC_LOGGING_SPEC=info
+
+function    usage {
+    echo  "Usage: . ./set-env.sh    ORG_NAME"
+    echo  "Sets the environment for the specific org"
+}
+
+export CURRENT_ORG_NAME=$1
+
+echo "Setting environment for $CURRENT_ORG_NAME"
+
+# Set environment variables based on the ORG_Name
+case $CURRENT_ORG_NAME in
+    "digibank")   export CORE_PEER_MSPCONFIGPATH=./fabric-ca/client/digibank/admin/msp
+              export CORE_PEER_ADDRESS=$DIGIBANK_EP
+              export CORE_PEER_LOCALMSPID=DigibankMSP
+        ;;
+    "magnetocorp") export CORE_PEER_MSPCONFIGPATH=./fabric-ca/client/magnetocorp/admin/msp
+              export CORE_PEER_ADDRESS=$MAGNETOCORP_EP
+              export CORE_PEER_LOCALMSPID=MagnetocorpMSP
+        ;;
+
+    *) usage
+esac
