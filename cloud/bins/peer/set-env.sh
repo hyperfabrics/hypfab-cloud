5a538bfc53132216ec5e48e503bcdeb7a563e282
pratikhyperfabrics <pratik.patil@hyperfabrics.com>
2019-06-03 09:44:46 -0400

    cloud enviorment setup

--- cloud/bins/peer/set-env.sh
+++ cloud/bins/peer/set-env.sh
@@ -2,7 +2,7 @@
 # Part of a online course. Please check it out at http://www.acloudfan.com
 
 # EDIT THIS To Control the Peer Setup
-export ORDERER_ADDRESS=18.220.118.116:7050
+export ORDERER_ADDRESS=18.224.25.94:7050
 
 export FABRIC_LOGGING_SPEC=info
 
