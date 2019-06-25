e9b549ed6f5d7df0bd2cf494fc7579c0f08ae60f
pratikhyperfabrics <pratik.patil@hyperfabrics.com>
2019-06-25 16:54:14 -0400

    cloud enviorment setup

--- cloud/bins/peer/create-commercialpaper-channel.sh
+++ cloud/bins/peer/create-commercialpaper-channel.sh
@@ -1,7 +1,7 @@
 # Copyright 2018 @ http://ACloudFan.com 
 # Part of a online course. Please check it out at http://www.acloudfan.com
 
-# Create the airline channel
+# Create the commercialpaper channel
 # Orderer Address env variable is getting set in the set-env.sh
 source  ./set-env.sh
 
@@ -9,4 +9,4 @@ source  ./set-env.sh
 export CHANNEL_TX_FILE=./commercialpaper-channel.tx
 
 # Execute the channel create command
-peer channel create -o $ORDERER_ADDRESS -c commercialpaperchannel -f $CHANNEL_TX_FILE
+peer channel create --catfile $TLS_CA_FILE --certfile $TLS_CERT_FILE --keyfile $TLS_KEY_FILE -o $ORDERER_ADDRESS --tls -c commercialpaperchannel -f $CHANNEL_TX_FILE
--- cloud/bins/peer/set-env.sh
+++ cloud/bins/peer/set-env.sh
@@ -2,7 +2,13 @@
 # Part of a online course. Please check it out at http://www.acloudfan.com
 
 # EDIT THIS To Control the Peer Setup
+export PEER_ORG=digibank
 export ORDERER_ADDRESS=18.220.137.253:7050
+
+export TLS_CA_FILE="./fabric-ca/client/digibank/tls/ca.crt"
+export TLS_CERT_FILE="./fabric-ca/client/digibank/tls/server.crt"
+export TLS_KEY_FILE="./fabric-ca/client/digibank/tls/server.key"
+
 
 export FABRIC_LOGGING_SPEC=info
 
@@ -10,6 +16,6 @@ export CORE_PEER_LOCALMSPID=DigibankMSP
 
 export CORE_PEER_ID=digibank-peer1
 
-export PEER_ORG=digibank
+
 # Admin identity used for commands
 export CORE_PEER_MSPCONFIGPATH=./fabric-ca/client/digibank/admin/msp
